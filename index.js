1dddeb94840ba2b2a442ee53e3badc6f45967f19
Abanum Olise <abanum00@gmail.com>
2023-07-08 23:39:27 +0100

    mobile carousel styling

---
+++ index.js
@@ -0,0 +1,18 @@
+let dotSelectors = document.querySelectorAll(".dots");
+let carouselSlides = document.querySelectorAll(".slide");
+
+for (let dotSelector of dotSelectors) {
+    dotSelector.addEventListener("click", () => {
+        for (let dotSelector of dotSelectors) {
+            dotSelector.classList.remove("selected");
+        }
+        let slideOwner = dotSelector.classList[1];        
+        dotSelector.classList.add("selected");
+        for (let slide of carouselSlides) {
+            slide.classList.remove("show");
+            if (slideOwner === slide.classList[1]) {                
+                slide.classList.add("show");                
+            }
+        }
+    })
+}
